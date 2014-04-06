d1125d4000c3538b0544e5180e7aea951a267afd
Saniazzz <sania0622@gmail.com>
2014-04-06 20:38:15 +0300

    Add getValidTurn() method

--- TicTacToe.py
+++ TicTacToe.py
@@ -23,6 +23,20 @@ class TicTacToe:
         print "Each number represents one of 9 cells from the board:"
         self.printBoard([2,3,4,5,6,7,8,9,10])
 
+    def getValidTurn(self, turn):
+        valid = False
+        while not valid:
+            try:
+                user = raw_input("Where to place " + turn + " (1-9)? ")
+                user = int(user)
+                if user >= 1 and user <= 9:
+                    return user-1
+                else:
+                    print "Invalid move! Please try again.\n"
+                    self.printInstr()
+            except Exception:
+                print user + " is not a valid move! Please try again.\n"
+
     def run(self):
         #printing instructions
         self.printInstr()
